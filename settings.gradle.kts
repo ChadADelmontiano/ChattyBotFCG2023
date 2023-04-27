9695768be05e2cc3f9323c99a9f09a1a566d6b3c
Chad <chad.a.delmont@gmail.com>
2023-04-27 19:56:56 -0400

    Added constatts for ame and birth year of FCG as well as the introduction print lines in the command line

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "ChattyBotFCG2023"
+
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+
+object Constants {
+    const val BOT_NAME = "Fresh Cut Grass"
+    const val CREATION_YEAR = 2023
+}
+fun main(args: Array<String>) {
+    println("Hello my name is ${Constants.BOT_NAME}!")
+    println("I was created in ${Constants.CREATION_YEAR}.")
+}
