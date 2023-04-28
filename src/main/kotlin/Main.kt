d3d6390e43bc6a1d6e1c26e2cd65599a71f9d609
Chad <chad.a.delmont@gmail.com>
2023-04-27 22:00:50 -0400

    implemented a scanner and FCG asks user for name

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,17 @@
-
+import java.util.Scanner
 object Constants {
     const val BOT_NAME = "Fresh Cut Grass"
     const val CREATION_YEAR = 2023
 }
 fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    //This is where ChattyBot(FCG) introduces themselves
     println("Hello my name is ${Constants.BOT_NAME}!")
     println("I was created in ${Constants.CREATION_YEAR}.")
+
+    //FCG asks the user their name
+    println("Could you remind me of your name again?")
+    val usersName = scanner.nextLine()
+
+    println("Oh that's right! Welcome back $usersName!")
 }
